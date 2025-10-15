ee1d19e6e44ef58d506b1f9726d879e3d95ea3dc
sidalope <bisi90@gmail.com>
2025-10-15 18:53:50 +0200

    add: Modify ft_atoi() to check for overflows

--- src/utils/ft_atoi.c
+++ src/utils/ft_atoi.c
@@ -3,49 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abisani <abisani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 12:55:28 by abisiani          #+#    #+#             */
-/*   Updated: 2025/06/03 13:54:16 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/10/15 18:50:56 by abisani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	skip_whitespace(const char *str)
-{
-	int	i;
+#include "../push_swap.h"
 
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
-		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-		i++;
-	return (i);
+static char	*skip_whitespace(char *str)
+{
+	while (*str == ' ' || *str == '\n' || *str == '\t'
+		|| *str == '\v' || *str == '\f' || *str == '\r')
+		str++;
+	return (str);
 }
 
-int	ft_atoi(const char *str)
+int	ft_atoi(char *str)
 {
-	int		i;
-	int		integer;
+	long	res;
 	int		minus;
 
-	i = skip_whitespace(str);
-	integer = 0;
+	str = skip_whitespace(str);
+	res = 0;
 	minus = 0;
-	if (str[i])
+	if (*str)
 	{
-		if (str[i] == '-')
+		if (*str == '-')
 		{
 			minus = 1;
-			i++;
+			str++;
 		}
-		else if (str[i] == '+')
-			i++;
-		while (str[i] >= '0' && str[i] <= '9')
+		else if (*str == '+')
+			str++;
+		while (*str >= '0' && *str <= '9')
 		{
-			integer = integer * 10 + str[i] - '0';
-			i++;
+			res = res * 10 + *str++ - '0';
+			if ((!minus && res > INT_MAX) || (minus && - res < INT_MIN))
+				return (ps_error(), 0);
 		}
 		if (minus)
-			integer *= -1;
+			res *= -1;
 	}
-	return (integer);
+	return ((int) res);
 }
