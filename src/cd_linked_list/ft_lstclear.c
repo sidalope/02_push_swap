9494596a6c519487fc70b19c9b43e00f03af44a2
sidalope <bisi90@gmail.com>
2025-10-21 16:05:05 +0200

    fix: Set freed pointer value to null bw iterations

--- src/cd_linked_list/ft_lstclear.c
+++ src/cd_linked_list/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: abisani <abisani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:19:25 by abisiani          #+#    #+#             */
-/*   Updated: 2025/10/20 13:34:03 by abisani          ###   ########.fr       */
+/*   Updated: 2025/10/21 15:08:23 by abisani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	ft_lstclear(t_list_node **current, t_list_node *head)
 		return ;
 	next = (*current)->next;
 	free(*current);
+	*current = NULL;
 	if (next == head)
 		return ;
 	ft_lstclear(&next, head);
