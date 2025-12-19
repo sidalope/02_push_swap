df6d8c4b3f026813b3ffc5e5606283481a4d997d
sidalope <bisi90@gmail.com>
2025-12-19 09:40:12 +0100

    fix: Remove single argument conditional exit
    push_swap was skipping type checks for singleton
    input, which resulted in accepting things like:
    "./push_swap g". Now Pass everything through
    init() input parser.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abisiani <abisiani@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:58:28 by abisani           #+#    #+#             */
-/*   Updated: 2025/12/18 14:49:39 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/12/19 09:37:12 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	main(int argc, char *argv[])
 {
 	t_stacks	stacks;
 
-	if (argc == 1 || argc == 2)
+	if (argc == 1)
 		return (0);
 	argc--;
 	init(argc, argv, &stacks);
